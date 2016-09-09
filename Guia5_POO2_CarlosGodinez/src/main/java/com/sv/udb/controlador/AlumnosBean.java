018dec31f5fdacc94b312bf390589bee153600f4
Carlos <Carlos@192.168.1.3>
2016-09-09 09:02:27 -0600

    DataTable con Bootstrap (pendiente modificar y eliminar)

--- Guia5_POO2_CarlosGodinez/src/main/java/com/sv/udb/controlador/AlumnosBean.java
+++ Guia5_POO2_CarlosGodinez/src/main/java/com/sv/udb/controlador/AlumnosBean.java
@@ -7,6 +7,9 @@ package com.sv.udb.controlador;
 
 import com.sv.udb.modelo.Alumnos;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.ManagedBean;
 import javax.annotation.PostConstruct;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
@@ -14,6 +17,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import org.primefaces.context.RequestContext;
 
 /**
@@ -22,9 +26,11 @@ import org.primefaces.context.RequestContext;
  */
 @Named(value = "alumnosBean")
 @ViewScoped
+@ManagedBean
 public class AlumnosBean implements Serializable{
     private Alumnos objeAlum;
     private boolean guardar;
+    private List<Alumnos> alumList = null;
 
     public Alumnos getObjeAlum() {
         return objeAlum;
@@ -36,6 +42,14 @@ public class AlumnosBean implements Serializable{
 
     public boolean isGuardar() {
         return guardar;
+    }
+
+    public List<Alumnos> getAlumList() {
+        return alumList;
+    }
+
+    public void setAlumList(List<Alumnos> alumList) {
+        this.alumList = alumList;
     }
     
     /**
@@ -50,6 +64,18 @@ public class AlumnosBean implements Serializable{
     {
         this.objeAlum = new Alumnos();
         this.guardar = true;
+        this.alumList = new ArrayList<>();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PARCIALPU");
+        EntityManager em = emf.createEntityManager();
+        try
+        {
+           TypedQuery<Alumnos> query =em.createNamedQuery("Alumnos.findAll", Alumnos.class);
+           this.alumList = query.getResultList();
+        }
+        catch(Exception ex)
+        {
+            
+        }
     }
     
     public void guar()
